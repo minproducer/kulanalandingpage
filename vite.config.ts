d3a8e50e0a8952f6b005db3e7e619bcc2cf05539
hmtravel <carshoian@gmail.com>
2025-11-02 15:28:02 +0700

    Update Vite config for better GitHub Pages compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
